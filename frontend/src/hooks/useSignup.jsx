669234f18ba666be0895bbc5c805d5964cccd067
Tommi <tommi.s.halla@gmail.com>
2024-11-27 10:03:43 +0200

    Signup refactored to use hook

---
+++ frontend/src/hooks/useSignup.jsx
@@ -0,0 +1,29 @@
+export default useSignup = ({ setIsAuthenticated }) => {
+  const navigate = useNavigate();
+
+  const signup = async (email, password) => {
+    try {
+      const response = await fetch("/api/users/signup", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ email, password }),
+      });
+
+      if (response.ok) {
+        const user = await response.json();
+        localStorage.setItem("user", JSON.stringify(user));
+        console.log("User signed up successfully!");
+        setIsAuthenticated(true);
+        navigate("/");
+      } else {
+        console.error("Signup failed", response);
+      }
+    } catch (error) {
+      console.error("Error during signup:", error);
+    }
+  };
+
+  return signup;
+}
